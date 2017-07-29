5a196c2d9971e370efdad4ae6d0fc002c3cccd0a
zander mackenzie <amacke201@caledonian.ac.uk>
2017-07-29 23:53:37 +0100

    content started

--- js/js.js
+++ js/js.js
@@ -1,7 +1,28 @@
 
 
+$(document).ready(function(){
 
-
-
-
-
+    
+    
+    $('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  accessibility:true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  arrows:false,
+  dots:true,
+  speed:1000
+        
+});
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
