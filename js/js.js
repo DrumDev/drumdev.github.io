b60875b17a6afe9e0b9f1e3dbca2ea51b4be7c13
zander mackenzie <amacke201@caledonian.ac.uk>
2017-10-04 00:18:12 +0100

    robots attempt

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
   $(function() {
-     $("a.smooth-scroll").click(function() {
+     $(".smooth-scroll").click(function() {
        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
          var target = $(this.hash);
          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
