411de3531de9f8050c750f4a0ed3ec8b3a3d943c
zander mackenzie <amacke201@caledonian.ac.uk>
2017-10-04 04:32:04 +0100

    slick

---
+++ js/js.js
@@ -0,0 +1,42 @@
+//myJS
+
+$(document).ready(function() {
+    
+  $(function() {
+     $(".smooth-scroll").click(function() {
+       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+         var target = $(this.hash);
+         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html, body').animate({
+             scrollTop: target.offset().top
+           }, 1000);
+           return false;
+         }
+       }
+     });
+   });
+    
+    
+            $('.our-work-slick').slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+  ]
+});
+    
+    
+});
