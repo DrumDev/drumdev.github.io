8757656839ce2116c75a60deffa4f44a440465c3
zander mackenzie <amacke201@caledonian.ac.uk>
2017-10-05 22:16:49 +0100

    wully info

--- js/js.js
+++ js/js.js
@@ -17,26 +17,24 @@ $(document).ready(function() {
      });
    });
     
-    
-            $('.our-work-slick').slick({
-  dots: true,
-  arrows: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
+  $('.our-work-slick').slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
       }
-    }
-  ]
-});
-    
+    ]
+  });
     
 });
